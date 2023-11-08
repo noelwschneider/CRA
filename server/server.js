f1e6402be1ced6e7fd4cecd5f9b2e88fad5e1d07
Noel Schneider <noelwschneider@gmail.com>
2023-11-08 01:46:32 -0600

    require dotenv in server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const exampleRouter = require('./routes/example.router.js')
 
 const port = process.env.PORT || 5000;
-
 
 
 /** ---------- MIDDLEWARE ---------- **/
